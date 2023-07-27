fd6d37226abfeacf9891dcda4fd2bcf17ce33e0a
Georgy <funnyfriendi@Mail.ru>
2023-07-27 14:13:08 +0600

    Сделал задание #13, показывает третье число или ошибку

---
+++ Task_013/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Пожалуйста, введите произвольное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+if (number < 100) {
+    Console.Write("У введенного числа нету 3 знака");
+} else {
+    Console.WriteLine(SecondNumber(number));
+}
+
+int SecondNumber(int number) {
+
+    while (number >= 1000) {
+            number /= 10;
+        }
+
+    int thirdDigit = number % 10;
+    return thirdDigit;
+};
