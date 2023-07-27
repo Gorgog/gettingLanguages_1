01f459145dd436de244db9cba64dea39ab1bcd77
Georgy <funnyfriendi@Mail.ru>
2023-07-27 13:49:13 +0600

    Сделал задание #10, показывает второе число в трехзначном числе

---
+++ Task_010/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Пожалуйста, введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+number = Math.Abs(number);
+
+if(number >= 100 && number <= 999){
+    Console.WriteLine(SecondNumber(number));
+} else {
+     Console.WriteLine("Число не является трехзначным");
+}
+
+int SecondNumber(int number) {
+    int secontNumber = (number / 10) % 10;
+    return secontNumber;
+};
