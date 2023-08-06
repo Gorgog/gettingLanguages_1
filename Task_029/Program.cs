7dbf16083be62b80fb516cdf8e51a259ab91b371
Georgy <funnyfriendi@Mail.ru>
2023-08-06 14:05:41 +0600

    Добавил задачу 29

---
+++ Task_029/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int len = GetInt("Длина массива: ");
+int start = GetInt("Начальное значение: ");
+int end = GetInt("Конечное значение: ");
+Console.WriteLine($"[{string.Join(", ", Generate(len, start, end))}]");
+
+int GetInt(string msg)
+{
+    int result;
+    Console.Write(msg);
+    while (!int.TryParse(Console.ReadLine(), out result))
+    {
+        Console.Write("Пожалуйста, введите корректное число: ");
+    }
+    return result;
+}
+
+int[] Generate(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+
