f7b96eae8ce84af1206c2cfd85bc2689f830e6c7
Georgy <funnyfriendi@Mail.ru>
2023-08-06 13:39:49 +0600

    Добавил задачу 25

---
+++ Task_025/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int baseNumber = GetInput("Введите основание: ");
+int powerValue = GetInput("Введите степень: ");
+
+if (IsValidPower(powerValue))
+    Console.WriteLine($"Число {baseNumber} в степени {powerValue} равно {CalculatePower(baseNumber, powerValue)}");
+
+int GetInput(string message) {
+    Console.Write(message);
+    
+    if (int.TryParse(Console.ReadLine(), out int parsedValue))
+        return parsedValue;
+    
+    Console.WriteLine("Введите корректное число.");
+    return GetInput(message);
+}
+
+int CalculatePower(int baseNumber, int powerValue) {
+    int result = 1;
+    for (int i = 0; i < powerValue; i++) result *= baseNumber;
+    return result;
+}
+
+bool IsValidPower(int powerValue) {
+    if(powerValue < 0) {
+        Console.WriteLine("Степень не должна быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
